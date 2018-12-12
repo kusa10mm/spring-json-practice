5581318d99e82a30f2a746d89030a51fd071b8a4
kusa10mm <hirotoshi.kusano@bizreach.co.jp>
2018-12-12 18:51:49 +0900

    MBPのコミット

--- src/main/java/com/example/springjsonpractice/controller/JsonReceiveController.java
+++ src/main/java/com/example/springjsonpractice/controller/JsonReceiveController.java
@@ -12,22 +12,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class JsonReceiveController {
 
-    @PostMapping("/json")
-    public ResponseEntity<JsonTestDTO> testMethod(@RequestBody JsonTestDTO jsonTestDTO) {
-        // JsonTestDTO postJsonTestDTO = new JsonTestDTO();
-        // postJsonTestDTO.setTestText("成功");
-        // return new ResponseEntity<>(postJsonTestDTO, HttpStatus.OK);
-        return new ResponseEntity<>(new JsonTestDTO(jsonTestDTO.getTestText()), HttpStatus.OK);
+    @GetMapping
+    public String testNormalGetMethod() {
+        return "Hello World";
     }
+
+    //セッターなしでjsonTestDTOに値がセットされているなら、なくても取れるということ
+    // @PostMapping()
+    // public String stringPostMethod(@RequestBody JsonTestDTO jsonTestDTO) {
+    //     // JsonTestDTO postJsonTestDTO = new JsonTestDTO();
+    //     // postJsonTestDTO.setTestText("成功");
+    //     // return new ResponseEntity<>(postJsonTestDTO, HttpStatus.OK);
+    //     // return "セッターなしで成功";
+    //     return jsonTestDTO.getTestText();
+    // }
 
     @GetMapping("/json")
     public ResponseEntity<JsonTestDTO> testGetMethod() {
         return new ResponseEntity<>(new JsonTestDTO(), HttpStatus.OK);
     }
 
-    @GetMapping
-    public String testNormalGetMethod() {
-        return "Hello World";
-    }
+    @PostMapping("/json")
+    public ResponseEntity<JsonTestDTO> testMethod(@RequestBody JsonTestDTO jsonTestDTO) {
+        // JsonTestDTO postJsonTestDTO = new JsonTestDTO();
+        // postJsonTestDTO.setTestText("成功");
+        // return new ResponseEntity<>(postJsonTestDTO, HttpStatus.OK);
+        // return "セッターなしで成功";
 
+        return new ResponseEntity<>(new JsonTestDTO(), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/springjsonpractice/controller/JsonTestDTO.java
+++ src/main/java/com/example/springjsonpractice/controller/JsonTestDTO.java
@@ -1,22 +1,20 @@
 package com.example.springjsonpractice.controller;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import java.io.Serializable;
 
 class JsonTestDTO implements Serializable {
 
     private String testText;
 
-    public String getTestText() {
-        return testText;
-    }
+    // public String getTestText() {
+    //     return testText;
+    // }
 
-    JsonTestDTO(){
-
-    }
-
-    JsonTestDTO(String postText) {
-        this.testText = postText;
-    }
+    // JsonTestDTO(String postText) {
+    //     this.testText = postText;
+    // }
 
     // public void setTestText(String testText) {
     //     this.testText = testText;
